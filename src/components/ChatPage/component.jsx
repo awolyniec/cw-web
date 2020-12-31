cd5391405e169489180b0f2e5ea61a4e4a22dee5
Alec Wolyniec <alecwolyniec@gmail.com>
2020-12-31 16:25:32 -0500

    feat: Add static visual version of sign-in view

--- src/components/ChatPage/component.jsx
+++ src/components/ChatPage/component.jsx
@@ -1,17 +1,46 @@
 import React from 'react';
+import { CirclePicker } from 'react-color';
 
 import './styles.scss';
 
 const ChatPage = () => {
 
+  const getChatView = () => {
+    return (
+      <div className="chat-view">
+        <div className="chat-section-container">
+
+        </div>
+        <div className="member-list-container">
+        </div>
+      </div>
+    );
+  };
+
+  // TODO: make into own page once accounts are created
+  const getSignInView = () => {
+    return (
+      <div className="sign-in-view">
+        <div className="sign-in-form">
+          <h1>Welcome to ChattyWatty</h1>
+          <span className="input-label">Enter your name:</span>
+          <input className="name-input" type="text" />
+          <span className="input-label">Pick your color:</span>
+          <div className="circle-picker-container">
+            <CirclePicker width={"400px"} />
+          </div>
+          <div>
+            <button className="chat-enter-button" type="button">Enter Chat</button>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="chat-page">
-      <div className="chat-section-container">
-
-      </div>
-      <div className="member-list-container">
-
-      </div>
+      {getSignInView()}
+      {/* {getChatView()} */}
     </div>
   );
 };
