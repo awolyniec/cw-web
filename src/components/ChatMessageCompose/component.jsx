897d53ff20af7c9c67bd1604980cc2f4ac2c5363
Alec Wolyniec <alecwolyniec@gmail.com>
2021-01-04 16:06:39 -0500

    feat: Have message composer send message when enter (but not enter + shift) is pressed, or when "Send" button is clicked

--- src/components/ChatMessageCompose/component.jsx
+++ src/components/ChatMessageCompose/component.jsx
@@ -2,12 +2,24 @@ import React from 'react';
 
 import './styles.scss';
 
-const ChatMessageCompose = () => {
-  
+const ChatMessageCompose = ({ sendMessage = () => {} }) => {
+  const handleKeypress = e => {
+    if(e.which === 13 && !e.shiftKey) {        
+        sendMessage();
+        e.preventDefault();
+    }
+}
+
   return (
     <div className="chat-message-compose">
-      <textarea rows={4} />
-      <button className="send-button" type="button">Send</button>
+      <textarea rows={4} onKeyPress={handleKeypress} />
+      <button
+        className="send-button"
+        type="button"
+        onClick={sendMessage}
+      >
+        Send
+      </button>
     </div>
   );
 };
